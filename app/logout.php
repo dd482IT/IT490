4c5fe9169893a25114d7b5c2558610a3035b04f6
Ubuntu <ubuntu@ip-172-31-29-51.ec2.internal>
2021-06-19 14:13:48 +0000

    Small Changes with require_once()

--- app/logout.php
+++ app/logout.php
@@ -4,7 +4,6 @@ session_start();
 session_unset();
 session_destroy();
 ?>
-<?php require_once(__DIR__ . "/nav.php"); ?>
 <?php
 
 die(header("Location: /app/login.php"));
--- app/nav.php
+++ app/nav.php
@@ -1,6 +1,6 @@
 <?php
-//TODO REQUIRE HELPER FUNCTIONS 
-?>
+require_once(__DIR__ . "/Functions/isLoggedIn.php");?>
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -15,15 +15,19 @@
       <li class="nav-item active">
         <a class="nav-link" href="/app/home.php">Home <span class="sr-only">(current)</span></a>
       </li>
+      <?php if (!is_logged_in()): ?>
       <li class="nav-item">
         <a class="nav-link" href="/app/login.php">Login</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/app/register.php">Register</a>
       </li>
+      <?php endif; ?>
+      <?php if (is_logged_in()): ?>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link disabled" href="Profile">Profile</a>
       </li>
+      <?php endif; ?>
     </ul>
   </div>
 </nav>

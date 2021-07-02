135bd6fb005225b0c22aa7a42d2e3e5d52f19d34
traymond22 <50150225+traymond22@users.noreply.github.com>
2021-07-02 16:11:07 -0400

    Create flash.php

---
+++ app/Functions/flash.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+function flash($msg) {
+    if (isset($_SESSION['flash'])) {
+        array_push($_SESSION['flash'], $msg);
+    }
+    else {
+        $_SESSION['flash'] = array();
+        array_push($_SESSION['flash'], $msg);
+    }
+
+}
+
+function getMessages() {
+    if (isset($_SESSION['flash'])) {
+        $flashes = $_SESSION['flash'];
+        $_SESSION['flash'] = array();
+        return $flashes;
+    }
+    return array();
+}
+
+
+
+?>
+<div class="container" id="flash">
+    <?php $messages = getMessages(); ?>
+    <?php if ($messages): ?>
+        <?php foreach ($messages as $msg): ?>
+            <div class="row bg-secondary justify-content-center">
+                <p><?php echo $msg; ?></p>
+            </div>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</div>
+<script>
+    //used to pretend the flash messages are below the first nav element
+    function moveMeUp(ele) {
+        let target = document.getElementsByTagName("nav")[0];
+        if (target) {
+            target.after(ele);
+        }
+    }
+
+    moveMeUp(document.getElementById("flash"));
+</script>

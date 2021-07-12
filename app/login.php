2f775d88f5610ca29e6940f074872097fb6bb006
traymond22 <50150225+traymond22@users.noreply.github.com>
2021-07-12 16:37:19 -0400

    Changes to login.php

--- app/login.php
+++ app/login.php
@@ -5,6 +5,43 @@ require_once(__DIR__ . "/Functions/safe_echo.php");
 session_start();
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; margin: 0 auto;}
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+        <form method="post">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="login" class="form-control">
+                
+            </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+            
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
+
+<!--
 <html>
 <head>
 
@@ -22,8 +59,9 @@ session_start();
 				</form>
 			</div>
 		</body>
-</head> 
+</head>
 <html>
+-->	
 
 <?php
 

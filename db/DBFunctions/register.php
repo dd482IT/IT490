45bd8b74aecbd5c9bef40eaae677a7cab3a212c8
ak2425-njit <49198431+ak2425-njit@users.noreply.github.com>
2021-07-21 14:28:19 -0400

    Update register.php
    Added try catch

--- db/DBFunctions/register.php
+++ db/DBFunctions/register.php
@@ -1,28 +1,33 @@
 <?php
 function register($username, $email, $password){
-	//from dbconnection.php
-	$stmt = getDB()->prepare("SELECT * FROM Users WHERE username = :username LIMIT 1");
-	$hash = password_hash($password, PASSWORD_BCRYPT);
-	$stmt->execute([":username" => $username]);
-	$result = $stmt->fetch(PDO::FETCH_ASSOC);
+        //from dbconnection.php
+        try{
+        $stmt = getDB()->prepare("SELECT * FROM Users WHERE username = :username LIMIT 1");
+        $hash = password_hash($password, PASSWORD_BCRYPT);
+        $stmt->execute([":username" => $username]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-	if($result){
-		return array("status"=>400, "message"=>"Username or Email already in use");
-	}
-	else{
-		$stmt_2 = getDB()->prepare("INSERT INTO Users(username, email, password) VALUES(:username, :email, :password)");
-		$stmt_2->execute(array(
-			":username" => $username,
-			":email" => $email,
-			":password" => $hash
-		));
-		$e = $stmt_2->errorInfo();
-		if ($e[0] == "00000"){
-			return array("status"=>200, "message"=>"Account Successfully Created");
-		}
-		else{
-			return array("status"=>400, "message"=>"do something");
-		}
-	}
+        if($result){
+                return array("status"=>400, "message"=>"Username or Email already in use");
+        }
+        else{
+                $stmt_2 = getDB()->prepare("INSERT INTO Users(username, email, password) VALUES(:username, :email, :password)");
+                $stmt_2->execute(array(
+                        ":username" => $username,
+                        ":email" => $email,
+                        ":password" => $hash
+                ));
+                $e = $stmt_2->errorInfo();
+                if ($e[0] == "00000"){
+                        return array("status"=>200, "message"=>"Account Successfully Created");
+                }
+                else{
+                        return array("status"=>400, "message"=>"do something");
+                }
+        }
+        }catch(Exception $e){
+                return array("status"=>400, "message"=>"Unsuccessful");
+        }
 }
 ?>
+

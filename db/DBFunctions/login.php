8f0523288c5e8f7bfd1ffa1d0225e09960f2519e
ak2425-njit <49198431+ak2425-njit@users.noreply.github.com>
2021-07-21 14:26:59 -0400

    Update login.php
    Added try catch

--- db/DBFunctions/login.php
+++ db/DBFunctions/login.php
@@ -1,38 +1,47 @@
 <?php
 function login($username, $password){
-	//from dbconnection.php
-	if(filter_var($username, FILTER_VALIDATE_EMAIL)){
-		echo "Valid Email";
-		$stmt = getDB()->prepare("SELECT * FROM Users WHERE email = :email LIMIT 1");
+        //from dbconnection.php
+        try{
+        if(filter_var($username, FILTER_VALIDATE_EMAIL)){
+                $stmt = getDB()->prepare("SELECT * FROM Users WHERE email = :email LIMIT 1");
         $stmt->execute(array(
                 ":email" => $username
         ));
-		$result = $stmt->fetch(PDO::FETCH_ASSOC);
-	}
-	else{
-		$stmt = getDB()->prepare("SELECT * FROM Users WHERE username = :username LIMIT 1");
-		$stmt->execute([":username"=>$username]);
-		$result = $stmt->fetch(PDO::FETCH_ASSOC);
-		
-		
-	}	
-	//TODO do proper checking, maybe user doesn't exist
-	if($result){
-		$hash = password_hash($password, PASSWORD_BCRYPT);
-		if(password_verify($password, $result["password"])){
-			unset($result["password"]);//never return password, there's no need to
-			return array("status"=>200, "user_id"=>$result["id"], "username"=>$result["username"], "email"=>$result["email"]);
-			//send user data back so app can use it
-		}
-		else{
-			return array("status"=>403, "message"=>"Incorrect Password");
-		}
-	}
-	else{
-		//must return a proper message so that the app can parse it
-		//and display a user friendly message to the user
-		return array("status"=>400, "message"=>"User doesn't exist");
-	}
+        $e = $stmt->errorInfo();
+        if ($e[0] != "00000") {
+            echo var_export($e, true);
+                }
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+        else{
+                $stmt = getDB()->prepare("SELECT * FROM Users WHERE username = :username LIMIT 1");
+                $stmt->execute([":username"=>$username]);
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+        }
+        //TODO do proper checking, maybe user doesn't exist
+        if($result){
+                $hash = password_hash($password, PASSWORD_BCRYPT);
+                if(password_verify($password, $result["password"])){
+                        unset($result["password"]);//never return password, there's no need to
+                        return array("status"=>200, "user_id"=>$result["id"], "username"=>$result["username"], "email"=>$result["email"]);
+                        //send user data back so app can use it
+                }
+                else{
+                        return array("status"=>403, "message"=>"Incorrect Password");
+                }
+        }
+        else{
+                //must return a proper message so that the app can parse it
+                //and display a user friendly message to the user
+                return array("status"=>400, "message"=>"User doesn't exist");
+        }
+        }catch(Exception $e){
+                return array("status"=>400, "message"=>"Unsuccessful");
+        }
 }
 ?>
+~
+
 

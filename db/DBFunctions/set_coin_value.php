fceff52e8b55d9230c91763c624086ff18b2e398
ak2425-njit <49198431+ak2425-njit@users.noreply.github.com>
2021-07-27 18:51:59 -0400

    Update set_coin_value.php

--- db/DBFunctions/set_coin_value.php
+++ db/DBFunctions/set_coin_value.php
@@ -1,44 +1,44 @@
 <?php
 
-function set_coin_value($coin_name, $coin_value){
+function set_coin_value($id, $coin_value){
         $date = date('Y/m/d H:i:s');
-        $id = 0;
-        switch($coin_name){
-                case "btc":
-                        $id = 1;
-                case "doge":
-                        $id = 2;
-                case "eth":
-                        $id = 3;
-
-        }
-        try{
+        $value = (float)$coin_value;
         $stmt = getDB()->prepare("SELECT * FROM CryptoCoins WHERE id = :id");
         $stmt->execute([":id"=>$id]);
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
         if($result){
-        $percent_change = getPercentChange($coin_value, $old_value);
-        $stmt = getDB()->prepare("UPDATE CryptoCoins SET (coin_value, percent_change, pull_date) VALUES (:coin_value, :percent_change, :pull_date) WHERE id = :id");
-        $stmt->execute(array(
-                ":coin_value" => $coin_value,
-                ":percent_change" => $percent_change,
-                ":pull_date" => $date,
-                ":id" => $id
-        ));
-        return array("status"=>"200", "message"=>"Successfully Updated");
+                $old_value = $result['coin_value'];
+                try{
+                $percent_change = (1 - $old_value/$value) * 100;
+                }catch(DivisionByZeroError $e){
+                        return array("status"=>403, "message"=>"Dividing by zero", "new_value"=>$value, "old_value"=>$result['coin_value']);
+                }
+                $stmt = getDB()->prepare("UPDATE CryptoCoins SET coin_value = :coin_value WHERE id = :id");
+                //try{
+                $stmt->execute(array(
+                        ":coin_value" => $value,
+                        ":id" => $id
+                ));
+                $stmt = getDB()->prepare("UPDATE CryptoCoins SET pull_date = :pull_date WHERE id = :id");
+                $stmt->execute(array(
+                        ":pull_date" => $date,
+                        ":id" => $id
+                ));
+                $stmt = getDB()->prepare("UPDATE CryptoCoins SET percent_change = :percent_change WHERE id = :id");
+                $stmt->execute(array(
+                        ":percent_change" => $percent_change,
+                        ":id" => $id
+                ));
+                //}catch(Exception $e){
+                //      return array("status"=>403, "message"=>"Unsuccessful", "new_value"=>$value, "old_value"=>$result['coin_value']);
+                //}
+                return array("status"=>"200", "message"=>"Successfully Updated");
         }
         else{
                 return array("status"=>"403", "message"=>"Unsupported CryptoCurrency");
         }
-        }catch(Exception $e){
-                return array("status"=>400, "message"=>"Unsuccessful");
-        }
 
-}
-
-function getPercentChange($new_value, $old_value){
-        $percent_change = ($new_value - $old_value)/$old_value;
-        return $percent_change;
 }
 
 ?>
+
